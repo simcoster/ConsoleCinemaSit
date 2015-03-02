c724ac427751b8a6a4ea8595d8e0889efb8abf35
simcoster <simcoster@gmail.com>
2015-03-02 23:56:43 +0200

    Now for the javascript!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,44 +7,69 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    private static Map<String, String> cookies ;
+    private static Map<String, String> mainSiteCookies;
+    static List<String> UrlList = new ArrayList<String>();
 
     public static void main(String[] args) throws IOException {
         // write your code here
-        Connection.Response response3 = Jsoup.connect("http://www.cinema-city.co.il/").method(Connection.Method.GET).execute();
-        cookies = response3.cookies(); //Get the ses
-         Jsoup.connect("http://www.cinema-city.co.il/").cookies(cookies)
-                .followRedirects(true)
-                .get();
-        String body2 = response3.body();
-        Document parse = Jsoup.parse(body2);
+        Connection.Response mainSiteResponse = Jsoup.connect("http://www.cinema-city.co.il/").method(Connection.Method.GET).execute();
+        mainSiteCookies = mainSiteResponse.cookies(); //Get the ses
+
+        String mainSiteBody = mainSiteResponse.body();
+        Document parse = Jsoup.parse(mainSiteBody);
+
         Elements select = parse.select("a[href][onclick^=openFancy]");
-        Pattern pattern = Pattern.compile("(openFancyBoxExternal'http://.*?')");
+        Pattern pattern = Pattern.compile("(openFancyBoxExternal\\('http://tickets.cinema-city.co.il.*?'\\))");
         Matcher matcher;
         for (final Element item : select) {
             matcher = pattern.matcher(item.toString());
-            if (matcher.find()) {
+            if (matcher.find() && item.toString().contains("סינמה")) {
                 String foundUrl = matcher.group(1);
-                System.out.println("blkanbld " + foundUrl);
-                //   GetMovieList(foundUrl.replace("'",""));
+                String edittedUrl = foundUrl.replace("openFancyBoxExternal('","").replace("')","").replace("&amp;","&");
+                UrlList.add(edittedUrl);
+                System.out.println("blkanbld " + edittedUrl);
             }
         }
 
-        Connection.Response response4 = Jsoup.connect("http://tickets.cinema-city.co.il//webtixsnetglilot/newsessionredirector.aspx?redirect=SelectBusinessDatePage.aspx&key=gli")
-                .cookies(cookies).method(Connection.Method.GET).execute();
-        cookies = response4.cookies();
+        for(final String url : UrlList)
+        {
+            GetMovieList(url);
+        }
 
-        //Connection.Response response2 = Jsoup.connect("http://tickets.cinema-city.co.il/webtixsnetglilot/SelectBusinessDatePage.aspx").method(Connection.Method.GET).execute();
-        //String body2 = response2.body();
-       // cookies = response2.cookies(); //Get the ses
-        Document doc = Jsoup.connect("http://tickets.cinema-city.co.il/WebTixsNetGlilot/TicketingTodaysEventsPage.aspx?BusinessDate=2015-2-27")
-                .cookies(cookies)
+
+
+
+
+    }
+
+    private static void GetMovieList(String url) throws IOException {
+        Map<String, String> ticketSiteCookies;
+     //   Connection.Response response4 = Jsoup.connect(url)
+        Connection.Response response4 = Jsoup.connect("http://tickets.cinema-city.co.il/webtixsnetglilot/newsessionredirector.aspx?redirect=SelectBusinessDatePage.aspx&key=gli")
+                .cookies(mainSiteCookies).method(Connection.Method.GET).execute();
+        ticketSiteCookies = response4.cookies();
+        Pattern pattern = Pattern.compile("(http://tickets.cinema-city.co.il/.*/)");
+        Matcher matcher= pattern.matcher(url);
+        if (!matcher.find())
+        {
+            return;
+        }
+        String baseTicketingUrl = matcher.group(1);
+
+        String datedTicketingUrl = GetDatedTicketingUrl(baseTicketingUrl, new Date());
+        Document doc = Jsoup.connect(datedTicketingUrl)
+                .cookies(ticketSiteCookies)
                 .followRedirects(true)
                 .get();
 
@@ -52,25 +77,17 @@ public class Main {
 
         Elements htmlContent = doc.select("a.CinemaSelectEventPage_Table_EventMasterCell");
         for (final Element item : htmlContent) {
-                System.out.println(item.text());
-//            Pattern pattern = Pattern.compile("('.*')");
-//            Matcher matcher;
-//            for (final Element item2 : htmlContent) {
-//                matcher = pattern.matcher(item.toString());
-//                if (matcher.find()) {
-//                    String foundUrl = matcher.group(1);
-//                    System.out.println("blkanbld " + foundUrl);
-//                    //   GetMovieList(foundUrl.replace("'",""));
-//                }
-//            }
-                //   GetMovieList(foundUrl.replace("'",""));
+            System.out.println(item.text());
         }
-
     }
 
-    private static void GetMovieList(String url) throws IOException {
-        Document doc = Jsoup.connect(url).cookies(cookies).followRedirects(true).get();
-        Element blah = doc.body();
-
+    private static String GetDatedTicketingUrl(String mainUrl, Date chosenDate)
+    {
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = dateFormat.format(date);
+        return mainUrl + "TicketingTodaysEventsPage.aspx?BusinessDate=" + formattedDate;
     }
+
+
 }
