f28d881d874c54bf9e0ecb4ee5bac7ae4bef3a90
simcoster <simcoster@gmail.com>
2015-03-03 03:13:22 +0200

    The mysterious bug! javascript gets null!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,13 @@
 package com.company;
 
+import com.gargoylesoftware.htmlunit.CookieManager;
+import com.gargoylesoftware.htmlunit.ScriptResult;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSpan;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -36,7 +44,7 @@ public class Main {
             matcher = pattern.matcher(item.toString());
             if (matcher.find() && item.toString().contains("סינמה")) {
                 String foundUrl = matcher.group(1);
-                String edittedUrl = foundUrl.replace("openFancyBoxExternal('","").replace("')","").replace("&amp;","&");
+                String edittedUrl = foundUrl.replace("openFancyBoxExternal('","").replace("')", "").replace("&amp;","&");
                 UrlList.add(edittedUrl);
                 System.out.println("blkanbld " + edittedUrl);
             }
@@ -55,14 +63,13 @@ public class Main {
 
     private static void GetMovieList(String url) throws IOException {
         Map<String, String> ticketSiteCookies;
-     //   Connection.Response response4 = Jsoup.connect(url)
+        //   Connection.Response response4 = Jsoup.connect(url)
         Connection.Response response4 = Jsoup.connect("http://tickets.cinema-city.co.il/webtixsnetglilot/newsessionredirector.aspx?redirect=SelectBusinessDatePage.aspx&key=gli")
                 .cookies(mainSiteCookies).method(Connection.Method.GET).execute();
         ticketSiteCookies = response4.cookies();
         Pattern pattern = Pattern.compile("(http://tickets.cinema-city.co.il/.*/)");
-        Matcher matcher= pattern.matcher(url);
-        if (!matcher.find())
-        {
+        Matcher matcher = pattern.matcher(url);
+        if (!matcher.find()) {
             return;
         }
         String baseTicketingUrl = matcher.group(1);
@@ -78,6 +85,52 @@ public class Main {
         Elements htmlContent = doc.select("a.CinemaSelectEventPage_Table_EventMasterCell");
         for (final Element item : htmlContent) {
             System.out.println(item.text());
+        }
+
+        final WebClient webClient = new WebClient();
+        CookieManager cookieManager = webClient.getCookieManager();
+        cookieManager.setCookiesEnabled(true);
+
+        for (Map.Entry<String, String> entry : ticketSiteCookies.entrySet())
+        {
+            Cookie cookie = new Cookie("tickets.cinema-city.co.il",entry.getKey(),entry.getValue());
+            cookieManager.addCookie(cookie);
+        }
+
+        webClient.setCookieManager(cookieManager);
+        HtmlPage page2 = webClient.getPage(datedTicketingUrl);
+        List<HtmlAnchor> byXPath = (List<HtmlAnchor>) page2.getByXPath("//a[@class='CinemaSelectEventPage_Events_DateTimeCell_Link']");
+        byXPath.remove(0);
+        byXPath.remove(1);
+
+        for (HtmlAnchor link : byXPath)
+        {
+            webClient.getPage(datedTicketingUrl);
+            String script = link.getHrefAttribute();
+            ScriptResult result = page2.executeJavaScript(script.replace("javascript:",""));
+            String s = result.toString();
+            Object javaScriptResult = result.getJavaScriptResult();
+            HtmlPage newPage = (HtmlPage)result.getNewPage(); //TODO get null here, why?
+            List<HtmlAnchor> movieName = (List<HtmlAnchor>) newPage.getByXPath("//a[@class='General_Result_Text']");
+            if (movieName.size()>0)
+                System.out.println(movieName.get(0).getFirstChild().toString());
+            List<HtmlSpan> timeOfDay = (List<HtmlSpan>) newPage.getByXPath("//span[@class='SessionInformation_Label_DateTime']");
+            if (timeOfDay.size()>0)
+            System.out.println(timeOfDay.get(0).getFirstChild().toString());
+            List<HtmlDivision> byXPath1 = (List<HtmlDivision>) newPage.getByXPath("//div[@class='seat']");
+            for (HtmlDivision element : byXPath1)
+            {
+                String message = "Seat " + element.getId().replace("_Seat_","") + " is ";
+                if (element.toString().contains("SeatStatus=1"))
+                {
+                    message+= "Vacant";
+                }
+                else
+                {
+                    message+= "Occupied";
+                }
+                 System.out.println(message);
+            }
         }
     }
 
